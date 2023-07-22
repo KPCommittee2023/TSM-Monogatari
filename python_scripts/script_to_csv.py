656fdfb6f959fc2e927da7b2137e85b0b1316066
AlphaBud75 <100396124+AlphaBud75@users.noreply.github.com>
2023-07-22 01:32:06 -0500

    py script works

---
+++ python_scripts/script_to_csv.py
@@ -0,0 +1,62 @@
+import csv, re
+from datetime import date
+
+
+def processTXT_to_CSV(chapterIndex,CHAPTER, chapter_fileName):
+    
+    starting_DialogCode_n = 1
+    Name_temp = ''
+
+    with open(
+        "python_scripts/script-csv-processed/MASTER-SCRIPT-"+ chapterIndex + CHAPTER + '-' + str(date.today()) + ".csv", 
+        "a", 
+        newline = ""
+    ) as csvfile:
+        fieldnames = [
+            '_DialogCode',
+            '_Chapter',
+            '_Name',
+            '_Comment',
+            '_Text'
+        ]
+        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        csv_writer.writeheader()
+
+        txt_open = open(
+            'python_scripts/script-txt-for-processing/'+chapter_fileName, 
+            'r',
+            encoding='UTF-8'
+        )
+        txt_lines = txt_open.readlines()
+
+        for single_line in txt_lines:
+            if single_line.strip() == '':
+                print(single_line)
+            elif  re.match( '^.+[:]$' ,single_line):
+                # print(single_line)
+                # csv_writer.writerow({
+                #     '_DialogCode' : 'CHAP1-'+str("{:05n}".format(starting_DialogCode_n)),
+                #     '_Chapter' : CHAPTER,
+                #     '_Name' : '',
+                #     '_Comment' : '',
+                #     '_Text' : ''
+                # })
+                Name_temp = single_line
+            else:
+                csv_writer.writerow({
+                    '_DialogCode' : 'CHAP1-'+str("{:05n}".format(starting_DialogCode_n))+'00',
+                    '_Chapter' : CHAPTER,
+                    '_Name' : Name_temp,
+                    '_Comment' : '',
+                    '_Text' : single_line
+                })
+            starting_DialogCode_n += 1
+                
+        
+
+processTXT_to_CSV(
+    chapterIndex= 'Chap0',
+    chapter_fileName = '00_prologue.txt',
+    CHAPTER = 'Prologue-A-Colorful-Entrance'
+)

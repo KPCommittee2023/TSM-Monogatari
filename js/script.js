7e746f48d50a61c9f357a2f7ec25b7b8d4299a91
AlphaBud75 <100396124+AlphaBud75@users.noreply.github.com>
2023-09-17 13:43:48 -0500

    scripts fmt

--- js/script.js
+++ js/script.js
@@ -1,194 +1,171 @@
 /* global monogatari */
 
 // Define the messages used in the game.
-monogatari.action ('message').messages ({
-	'Help': {
-		title: 'Help',
-		subtitle: 'Some useful Links',
-		body: `
+monogatari.action("message").messages({
+  Help: {
+    title: "Help",
+    subtitle: "Some useful Links",
+    body: `
 			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
 			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
-		`
-	}
+		`,
+  },
 });
 
 // Define the notifications used in the game
-monogatari.action ('notification').notifications ({
-	'Welcome': {
-		title: 'Welcome',
-		body: 'This is the Monogatari VN Engine',
-		icon: ''
-	}
+monogatari.action("notification").notifications({
+  Welcome: {
+    title: "Welcome",
+    body: "This is the Monogatari VN Engine",
+    icon: "",
+  },
 });
 
 // Define the Particles JS Configurations used in the game
-monogatari.action ('particles').particles ({
-
-});
+monogatari.action("particles").particles({});
 
 // Define the canvas objects used in the game
-monogatari.action ('canvas').objects ({
-
-});
+monogatari.action("canvas").objects({});
 
 // Credits of the people involved in the creation of this awesome game
-monogatari.configuration ('credits', {
-
-});
-
+monogatari.configuration("credits", {});
 
 // Define the images that will be available on your game's image gallery
-monogatari.assets ('gallery', {
-
-});
+monogatari.assets("gallery", {});
 
 // Define the music used in the game.
-monogatari.assets ('music', {
-
-});
+monogatari.assets("music", {});
 
 // Define the voice files used in the game.
-monogatari.assets ('voices', {
-
-});
+monogatari.assets("voices", {});
 
 // Define the sounds used in the game.
-monogatari.assets ('sounds', {
-
-});
+monogatari.assets("sounds", {});
 
 // Define the videos used in the game.
-monogatari.assets ('videos', {
-
-});
+monogatari.assets("videos", {});
 
 // Define the images used in the game.
-monogatari.assets ('images', {
-
-});
+monogatari.assets("images", {});
 
 // Define the backgrounds for each scene.
-monogatari.assets ('scenes', {
-
-});
-
+monogatari.assets("scenes", {});
 
 // Define the Characters
-monogatari.characters ({
-	'y': {
-		name: 'Yui',
-		color: '#5bcaff'
-	},
-	'character_object_player': {
-		name: '',
-		color: '#2E72C1'
-	},
-	'character_object_kyo': {
-		name: 'Kyo',
-		color: '#005766'
-	},
-	'character_object_atria': {
-		name: 'Atria',
-		color: '#912756'
-	},
-	'character_object_girl_a': {
-		name: 'Girl A',
-		color: '#2F2F30'
-	},
-	'character_object_girl_b': {
-		name: 'Girl B',
-		color: '#2F2F30'
-	},
-	'character_object_skye': {
-		name: 'Skye',
-		color: '#890F0F'
-	},
-	'character_object_student_a': {
-		name: 'Student A',
-		color: '#2F2F30'
-	},
-	'character_object_student_b': {
-		name: 'Student B',
-		color: '#2F2F30'
-	},
-	'character_object_student_c': {
-		name: 'Student C',
-		color: '#2F2F30'
-	},
-	'character_object_student_d': {
-		name: 'Student D',
-		color: '#2F2F30'
-	},
-	'character_object_esmeray': {
-		name: 'Esmeray',
-		color: '#274554'
-	},
-	'character_object_student': {
-		name: 'Student',
-		color: '#2F2F30'
-	}
+monogatari.characters({
+  y: {
+    name: "Yui",
+    color: "#5bcaff",
+  },
+  character_object_player: {
+    name: "",
+    color: "#2E72C1",
+  },
+  character_object_kyo: {
+    name: "Kyo",
+    color: "#005766",
+  },
+  character_object_atria: {
+    name: "Atria",
+    color: "#912756",
+  },
+  character_object_girl_a: {
+    name: "Girl A",
+    color: "#2F2F30",
+  },
+  character_object_girl_b: {
+    name: "Girl B",
+    color: "#2F2F30",
+  },
+  character_object_skye: {
+    name: "Skye",
+    color: "#890F0F",
+  },
+  character_object_student_a: {
+    name: "Student A",
+    color: "#2F2F30",
+  },
+  character_object_student_b: {
+    name: "Student B",
+    color: "#2F2F30",
+  },
+  character_object_student_c: {
+    name: "Student C",
+    color: "#2F2F30",
+  },
+  character_object_student_d: {
+    name: "Student D",
+    color: "#2F2F30",
+  },
+  character_object_esmeray: {
+    name: "Esmeray",
+    color: "#274554",
+  },
+  character_object_student: {
+    name: "Student",
+    color: "#2F2F30",
+  },
 });
 
-monogatari.script ({
-	// The game starts here.
-	'Start': [
-		'show scene #f7f6f6 with fadeIn',
-		'show notification Welcome',
-		{
-			'Input': {
-				'Text': 'What is your name?',
-				'Validation': function (input) {
-					return input.trim ().length > 0;
-				},
-				'Save': function (input) {
-					this.storage ({
-						player: {
-							name: input
-						}
-					});
-					return true;
-				},
-				'Revert': function () {
-					this.storage ({
-						player: {
-							name: ''
-						}
-					});
-				},
-				'Warning': 'You must enter a name!'
-			}
-		},
-		'y Hi {{player.name}} Welcome to Monogatari!',
-		{
-			'Choice': {
-				'Dialog': 'y Have you already read some documentation?',
-				'Yes': {
-					'Text': 'Yes',
-					'Do': 'jump Yes'
-				},
-				'No': {
-					'Text': 'No',
-					'Do': 'jump No'
-				}
-			}
-		}
-	],
+monogatari.script({
+  // The game starts here.
+  Start: [
+    "show scene #f7f6f6 with fadeIn",
+    "show notification Welcome",
+    {
+      Input: {
+        Text: "What is your name?",
+        Validation: function (input) {
+          return input.trim().length > 0;
+        },
+        Save: function (input) {
+          this.storage({
+            player: {
+              name: input,
+            },
+          });
+          return true;
+        },
+        Revert: function () {
+          this.storage({
+            player: {
+              name: "",
+            },
+          });
+        },
+        Warning: "You must enter a name!",
+      },
+    },
+    "y Hi {{player.name}} Welcome to Monogatari!",
+    {
+      Choice: {
+        Dialog: "y Have you already read some documentation?",
+        Yes: {
+          Text: "Yes",
+          Do: "jump Yes",
+        },
+        No: {
+          Text: "No",
+          Do: "jump No",
+        },
+      },
+    },
+  ],
 
-	'Yes': [
-		'y Thats awesome!',
-		'y Then you are ready to go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
-	],
+  Yes: [
+    "y Thats awesome!",
+    "y Then you are ready to go ahead and create an amazing Game!",
+    "y I can’t wait to see what story you’ll tell!",
+    "end",
+  ],
 
-	'No': [
+  No: [
+    "y You can do it now.",
 
-		'y You can do it now.',
+    "show message Help",
 
-		'show message Help',
-
-		'y Go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
-	]
+    "y Go ahead and create an amazing Game!",
+    "y I can’t wait to see what story you’ll tell!",
+    "end",
+  ],
 });

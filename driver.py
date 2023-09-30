d72a5707395de76bf57eeaaedbffdcee44f91197
alphabud75 <stephenzhou41@gmail.com>
2023-09-30 15:44:25 -0500

    rename

--- driver.py
+++ driver.py
@@ -5,16 +5,49 @@ from python_scripts.script_to_csv import processTXT_to_CSV
 
 if __name__ == '__main__':
     start_path = 'python_scripts'
+    txt_file_names = []
+    csv_file_names = []
+    print('\n reading files... \n')
+
     for root, dirs, files in os.walk(start_path):
         if 'ARCHIVED' in root:
             pass
-        else:
+        elif 'txt' in root:
+
             level = root.replace(start_path, '').count(os.sep)
             indent = ' ' * 4 * level
             print('{}{}/'.format(indent, os.path.basename(root)))
             sub_indent = ' ' * 4 * (level + 1)
             for f in files:
                 print('{}{}'.format(sub_indent, f))
+                txt_file_names.append(f)
+
+        elif 'csv' in root:
+
+            level = root.replace(start_path, '').count(os.sep)
+            indent = ' ' * 4 * level
+            print('{}{}/'.format(indent, os.path.basename(root)))
+            sub_indent = ' ' * 4 * (level + 1)
+            for f in files:
+                print('{}{}'.format(sub_indent, f))
+                csv_file_names.append(f)
+
+        else:
+            # level = root.replace(start_path, '').count(os.sep)
+            # indent = ' ' * 4 * level
+            # print('{}{}/'.format(indent, os.path.basename(root)))
+            # sub_indent = ' ' * 4 * (level + 1)
+            # for f in files:
+            #     print('{}{}'.format(sub_indent, f))
+            pass
+
+    print(
+        '\nfiles read: ',
+        '\n\ttxt_file_names read:\n\t\t',
+        '\n\t\t'.join(txt_file_names),
+        '\n\tcsv_file_names read:\n\t\t',
+        '\n\t\t'.join(csv_file_names)
+    )
 
     print('\n\n\n==================\n\n\n')
 

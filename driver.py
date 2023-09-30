13a8306a048831f9eca17ea0a4594f8e43db5218
alphabud75 <stephenzhou41@gmail.com>
2023-09-30 15:12:58 -0500

    fmt, rename files

--- driver.py
+++ driver.py
@@ -6,8 +6,25 @@ from python_scripts.script_to_csv import processTXT_to_CSV
 if __name__ == '__main__':
     start_path = 'python_scripts'
     for root, dirs, files in os.walk(start_path):
+        if 'ARCHIVED' in root:
+            pass
+        else:
+            level = root.replace(start_path, '').count(os.sep)
+            indent = ' ' * 4 * level
+            print('{}{}/'.format(indent, os.path.basename(root)))
+            sub_indent = ' ' * 4 * (level + 1)
+            for f in files:
+                print('{}{}'.format(sub_indent, f))
+
+    print('\n\n\n==================\n\n\n')
+
+    start_path = 'js'
+    for root, dirs, files in os.walk(start_path):
         level = root.replace(start_path, '').count(os.sep)
         indent = ' ' * 4 * level
         print('{}{}/'.format(indent, os.path.basename(root)))
+        sub_indent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(sub_indent, f))
 
     print('\n\n\n==================\n\n\n')
--- python_scripts/script_to_csv.py
+++ python_scripts/script_to_csv.py
@@ -2,16 +2,15 @@ import csv, re
 from datetime import date
 
 
-def processTXT_to_CSV(chapterIndex,CHAPTER, chapter_fileName):
-    
+def processTXT_to_CSV(chapterIndex, CHAPTER, chapter_fileName):
     starting_DialogCode_n = 1
-    Name_temp = ''
+    name_temp = ''
 
     with open(
-        "python_scripts/Sep17/script-csv-processed/MASTER-SCRIPT-"+ 
-            chapterIndex + CHAPTER + '-' + str(date.today()) + ".csv", 
-        "a", 
-        newline = ""
+            "python_scripts/Sep17/script-csv-processed/MASTER-SCRIPT-" +
+            chapterIndex + CHAPTER + '-' + str(date.today()) + ".csv",
+            "a",
+            newline=""
     ) as csvfile:
         fieldnames = [
             '_DialogCode',
@@ -25,7 +24,7 @@ def processTXT_to_CSV(chapterIndex,CHAPTER, chapter_fileName):
         csv_writer.writeheader()
 
         txt_open = open(
-            'python_scripts/Sep17/script-txt-for-processing/'+chapter_fileName, 
+            'python_scripts/Sep17/script-txt-for-processing/' + chapter_fileName,
             'r',
             encoding='utf_8'
         )
@@ -33,55 +32,53 @@ def processTXT_to_CSV(chapterIndex,CHAPTER, chapter_fileName):
 
         for single_line in txt_lines:
             if single_line.strip() == '':
-                pass                
-            elif  re.match( '^.+[:]$' ,single_line):
-                Name_temp = single_line
-            elif  re.match( '^[[].+$' ,single_line):
+                pass
+            elif re.match('^.+[:]$', single_line):
+                name_temp = single_line
+            elif re.match('^[[].+$', single_line):
                 csv_writer.writerow({
-                    '_DialogCode' : chapterIndex+str("{:05n}".format(starting_DialogCode_n))+'00',
-                    '_Chapter' : CHAPTER,
-                    '_Name' : single_line,
-                    '_Comment' : '',
-                    '_Text' : single_line
+                    '_DialogCode': chapterIndex + str("{:05n}".format(starting_DialogCode_n)) + '00',
+                    '_Chapter': CHAPTER,
+                    '_Name': single_line,
+                    '_Comment': '',
+                    '_Text': single_line
                 })
             else:
                 csv_writer.writerow({
-                    '_DialogCode' : chapterIndex+str("{:05n}".format(starting_DialogCode_n))+'00',
-                    '_Chapter' : CHAPTER,
-                    '_Name' : Name_temp,
-                    '_Comment' : '',
-                    '_Text' : single_line
+                    '_DialogCode': chapterIndex + str("{:05n}".format(starting_DialogCode_n)) + '00',
+                    '_Chapter': CHAPTER,
+                    '_Name': name_temp,
+                    '_Comment': '',
+                    '_Text': single_line
                 })
             starting_DialogCode_n += 1
-                
-        
 
 # processTXT_to_CSV(
 #     chapterIndex= 'Chapter_0-',
-#     chapter_fileName = '00_prologue.txt',
+#     chapter_fileName = 'Chapter_0_Prologue_Waxing_Iridescent.txt',
 #     CHAPTER = 'Prologue-A-Colorful-Entrance'
 # )
 #
 # processTXT_to_CSV(
 #     chapterIndex= 'Chapter_1-',
-#     chapter_fileName = '01.txt',
+#     chapter_fileName = 'Chapter_1_Healing_the_Conditioned_Puppet.txt',
 #     CHAPTER = 'The-Conditioned-Puppet'
 # )
 #
 # processTXT_to_CSV(
 #     chapterIndex= 'Chapter_2-',
-#     chapter_fileName = '02.txt',
+#     chapter_fileName = 'Chapter_2_Befriending_the_Shrewd_Dreamer.txt',
 #     CHAPTER = 'Shrewd-Dreamer'
 # )
 #
 # processTXT_to_CSV(
 #     chapterIndex= 'Chapter_3-',
-#     chapter_fileName = '03.txt',
+#     chapter_fileName = 'Chapter_3_Confronting_the_Moon_Prince.txt',
 #     CHAPTER = 'The-Moon-Prince'
 # )
 #
 # processTXT_to_CSV(
 #     chapterIndex= 'Chapter_epi-',
-#     chapter_fileName = '04_epilogue.txt',
+#     chapter_fileName = 'Chapter_E_Epilogue_Waning_Iridescent.txt',
 #     CHAPTER = 'Epilogue-A-Prismatic-Farewell'
 # )

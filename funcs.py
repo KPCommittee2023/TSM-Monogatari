f29b5260d16d07d146dcb000f378bd9396bcabdf
sunfili-side-up <sunfilis.dev@gmail.com>
2023-10-07 00:53:34 -0400

    finish python scripts

--- funcs.py
+++ funcs.py
@@ -1,21 +1,74 @@
+import os
+import csv, re
+
 # function definitions
 
-def read_txt_write_to_csv(txt_file_name):
-    print(txt_file_name)
+def read_txt_write_to_csv(txt_file_name, csv_file_directory_path):
+    starting_DialogCode_n = 1
+    name_temp = ""
 
-    txt_open = open(
-        txt_file_name,
-        "r",
-        encoding="utf_8",
-    )
-    txt_lines = txt_open.readlines()
+    # some operations on the file name to get chapter index and chapter name
+    txt_file_basename = os.path.basename(txt_file_name)
+    file_basename = txt_file_basename.replace("txt", "csv")
+    split_basename = (file_basename.split(".")[0]).split("_")
+    chapterIndex = "Chapter_" + split_basename[1] + "-"
+    chapter_name_list = split_basename[2:]
+    CHAPTER = '-'.join(chapter_name_list)
 
-    for single_line in txt_lines:
-        # print(single_line)
-        pass
-        # TO DO : implement the function to be taken   ((chapterIndex, CHAPTER, chapter_fileName)))paramters, passed from driver.py, and genreate csv files
+    # create the csv dir if it doesn't already exist
+    if not os.path.exists(csv_file_directory_path):
+        os.makedirs(csv_file_directory_path)
 
+    with open(
+        csv_file_directory_path
+        + "/"
+        + file_basename,
+        "w",
+        newline=""
+    ) as csvfile:
+        fieldnames = ["_DialogCode", "_Chapter", "_Name", "_Comment", "_Text"]  # creates csv columns
+        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # creates an object that writes to the csv file
 
+        csv_writer.writeheader()
+
+        txt_open = open(
+            txt_file_name,
+            "r",
+            encoding="utf_8",
+        )
+        txt_lines = txt_open.readlines()
+
+        for single_line in txt_lines:
+            if single_line.strip() == "":
+                pass
+            elif re.match("^.+[:]$", single_line):
+                name_temp = single_line
+            elif re.match("^[\[].+$", single_line):
+                csv_writer.writerow(
+                    {
+                        "_DialogCode": chapterIndex
+                        + str("{:05n}".format(starting_DialogCode_n))
+                        + "00",
+                        "_Chapter": CHAPTER,
+                        "_Name": single_line,
+                        "_Comment": "",
+                        "_Text": single_line,
+                    }
+                )
+            else:
+                csv_writer.writerow(
+                    {
+                        "_DialogCode": chapterIndex
+                        + str("{:05n}".format(starting_DialogCode_n))
+                        + "00",
+                        "_Chapter": CHAPTER,
+                        "_Name": name_temp,
+                        "_Comment": "",
+                        "_Text": single_line,
+                    }
+                )
+            starting_DialogCode_n += 1
+       
 def read_txt_write_to_csv0(chapterIndex, CHAPTER, chapter_fileName):
     starting_DialogCode_n = 1
     name_temp = ""
@@ -73,52 +126,40 @@ def read_txt_write_to_csv0(chapterIndex, CHAPTER, chapter_fileName):
                 )
             starting_DialogCode_n += 1
 
+def read_csv_return_txt_format(filename, dir_path):
 
+    csv_file_basename = os.path.basename(filename)
+    file_basename = csv_file_basename.replace("csv", "txt")
+    
+    # create the csv dir if it doesn't already exist
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-def read_csv_return_json_format(filename):
-     # TO DO : implement the function to be taken   ((chapterIndex, CHAPTER, chapter_fileName)))paramters, passed from driver.py, and genreate csv files
-    # referece this function in drvier.py
-
-    json_file_write = open(
-        "python_scripts/Sep17/processed-JSON/" + filename + ".json", "w"
+    txt_file_write = open(
+        dir_path + "/" + file_basename, "w"
     )
 
     with open(
-        "python_scripts/Sep17/script-csv-processed/" + filename + ".csv",
+        filename,
         "r",
-        # encoding="utf_8",
     ) as file:
         csvFile = csv.reader(file)
         for row in csvFile:
-            print(row)
-            json_file_write.write(' " ')
-            json_file_write.write(
+            txt_file_write.write(' " ')
+            txt_file_write.write(
                 row[2]
                 .replace("[", "")
                 .replace("]", "")
                 .replace(":", "")
                 .replace("\n", " ")
             )
-            json_file_write.write(" ")
-            json_file_write.write(
+            txt_file_write.write(" ")
+            txt_file_write.write(
                 row[4]
                 .replace("[", "")
                 .replace("]", "")
                 .replace("'", "\\'")
                 .replace("\n", " ")[:-1]
             )
-            json_file_write.write(' " ')
-            json_file_write.writelines(",\n\n")
+            txt_file_write.write(' " ')
+            txt_file_write.writelines(",\n\n")

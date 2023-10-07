a7758b07df011efcfd7af8e9f2d00cadbd99ea16
alphabud75 <stephenzhou41@gmail.com>
2023-10-07 13:45:33 -0500

    gitignore good. csvToJsonRULES good

--- csv_to_json_RULES.py
+++ csv_to_json_RULES.py
@@ -2,7 +2,7 @@ def do_differently_given_code(dialog_code):
     
     if dialog_code == 'Chapter_1-0000100':
         print('4')
-        return ' '
+        return ' ' # cannot return empty str. '' is falsy value in python
         pass
     elif False:
         pass

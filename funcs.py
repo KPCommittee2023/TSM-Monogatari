321fbf224551cecff0ba4e7084717fa7c96af9b8
alphabud75 <stephenzhou41@gmail.com>
2023-10-08 01:11:45 -0500

    will insert dialog no for debugging

--- funcs.py
+++ funcs.py
@@ -70,62 +70,58 @@ def read_txt_write_to_csv(txt_file_name, csv_file_directory_path):
                 )
             starting_DialogCode_n += 1
        
-def read_txt_write_to_csv0(chapterIndex, CHAPTER, chapter_fileName):
-    starting_DialogCode_n = 1
-    name_temp = ""
-
-    with open(
-        "python_scripts/Sep17/script-csv-processed/MASTER-SCRIPT-"
-        + chapterIndex
-        + CHAPTER
-        + "-"
-        # + str(date.today())
-        + ".csv",
-        "a",
-        newline="",
-    ) as csvfile:
-        fieldnames = ["_DialogCode", "_Chapter", "_Name", "_Comment", "_Text"]  # creates csv columns
-        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # creates an object that writes to the csv file
-
-        csv_writer.writeheader()
-
-        txt_open = open(
-            "python_scripts/Sep17/script-txt-for-processing/" + chapter_fileName,
-            "r",
-            encoding="utf_8",
-        )           # in charge of reading the txt file... 
-        txt_lines = txt_open.readlines() # ...line by line
-
-        for single_line in txt_lines:
-            if single_line.strip() == "":
-                pass
-            elif re.match("^.+[:]$", single_line):
-                name_temp = single_line
-            elif re.match("^[[].+$", single_line):
-                csv_writer.writerow(
-                    {
-                        "_DialogCode": chapterIndex
-                        + str("{:05n}".format(starting_DialogCode_n))
-                        + "00",
-                        "_Chapter": CHAPTER,
-                        "_Name": single_line,
-                        "_Comment": "",
-                        "_Text": single_line,
-                    }
-                )
-            else:
-                csv_writer.writerow(
-                    {
-                        "_DialogCode": chapterIndex
-                        + str("{:05n}".format(starting_DialogCode_n))
-                        + "00",
-                        "_Chapter": CHAPTER,
-                        "_Name": name_temp,
-                        "_Comment": "",
-                        "_Text": single_line,
-                    }
-                )
-            starting_DialogCode_n += 1
+# def read_txt_write_to_csv0(chapterIndex, CHAPTER, chapter_fileName):
+#     starting_DialogCode_n = 1
+#     name_temp = ""
+#     with open(
+#         "python_scripts/Sep17/script-csv-processed/MASTER-SCRIPT-"
+#         + chapterIndex
+#         + CHAPTER
+#         + "-"
+#         # + str(date.today())
+#         + ".csv",
+#         "a",
+#         newline="",
+#     ) as csvfile:
+#         fieldnames = ["_DialogCode", "_Chapter", "_Name", "_Comment", "_Text"]  # creates csv columns
+#         csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # creates an object that writes to the csv file
+#         csv_writer.writeheader()
+#         txt_open = open(
+#             "python_scripts/Sep17/script-txt-for-processing/" + chapter_fileName,
+#             "r",
+#             encoding="utf_8",
+#         )           # in charge of reading the txt file... 
+#         txt_lines = txt_open.readlines() # ...line by line
+#         for single_line in txt_lines:
+#             if single_line.strip() == "":
+#                 pass
+#             elif re.match("^.+[:]$", single_line):
+#                 name_temp = single_line
+#             elif re.match("^[[].+$", single_line):
+#                 csv_writer.writerow(
+#                     {
+#                         "_DialogCode": chapterIndex
+#                         + str("{:05n}".format(starting_DialogCode_n))
+#                         + "00",
+#                         "_Chapter": CHAPTER,
+#                         "_Name": single_line,
+#                         "_Comment": "",
+#                         "_Text": single_line,
+#                     }
+#                 )
+#             else:
+#                 csv_writer.writerow(
+#                     {
+#                         "_DialogCode": chapterIndex
+#                         + str("{:05n}".format(starting_DialogCode_n))
+#                         + "00",
+#                         "_Chapter": CHAPTER,
+#                         "_Name": name_temp,
+#                         "_Comment": "",
+#                         "_Text": single_line,
+#                     }
+#                 )
+#             starting_DialogCode_n += 1
 
 def read_csv_return_txt_format(csv_filename, dir_path):
     # parsing csv to json for script.js to read. Oct 7
@@ -162,6 +158,8 @@ def read_csv_return_txt_format(csv_filename, dir_path):
                     .replace(":", "")
                     .replace("\n", " ")
                 )
+                txt_file_write.write(" ")
+                txt_file_write.write(row[0])    # del this line in actual release
                 txt_file_write.write(" ")
                 txt_file_write.write(
                     row[4]

5e9465d5fc08ae999efed784c597bb83909ed383
alphabud75 <alphabud75>
2023-12-29 19:31:13 +0800

    sprite ep0

--- js/script.js
+++ js/script.js
@@ -46,7 +46,11 @@ monogatari.assets("sounds", {});
 monogatari.assets("videos", {});
 
 // Define the images used in the game.
-monogatari.assets("images", {});
+monogatari.assets("images", {
+
+  'radio_club_poster': 'RadioClub.png',
+  'wallposter': 'wallposter.png',
+});
 
 
 // Define the backgrounds for each scene.
@@ -149,7 +153,7 @@ monogatari.characters({
     color: "#2E72C1",
   },
   'character_object_narration': {
-    name: "_Narration",
+    name: "Narration",
     color: "#2E72C1",
   },
   'character_object_girl_a': {
@@ -363,40 +367,40 @@ monogatari.script({
 
 
 
-    {
-      'Choice': {
-        'Dialog': 'Jump to certain chapter? \
-				<br>\
-				 (This for developer to use. Delete in actual relesase.)\
-				<br>\
-				 (top of script.js)',
+    // {
+    //   'Choice': {
+    //     'Dialog': 'Jump to certain chapter? \
+		// 		<br>\
+		// 		 (This for developer to use. Delete in actual relesase.)\
+		// 		<br>\
+		// 		 (top of script.js)',
 
-        'CHAPTER_0': {
-          'Text': 'CHAPTER_0',
-          'Do': 'jump CHAPTER_0'
-        },
-        'CHAPTER_1': {
-          'Text': 'CHAPTER_1',
-          'Do': 'jump CHAPTER_1'
-        },
-        'CHAPTER_2': {
-          'Text': 'CHAPTER_2',
-          'Do': 'jump CHAPTER_2'
-        },
-        'CHAPTER_3': {
-          'Text': 'CHAPTER_3',
-          'Do': 'jump CHAPTER_3'
-        },
-        'CHAPTER_E': {
-          'Text': 'CHAPTER_E',
-          'Do': 'jump CHAPTER_E'
-        },
-        'END': {
-          'Text': 'END',
-          'Do': 'jump END'
-        }
-      }
-    },
+    //     'CHAPTER_0': {
+    //       'Text': 'CHAPTER_0',
+    //       'Do': 'jump CHAPTER_0'
+    //     },
+    //     'CHAPTER_1': {
+    //       'Text': 'CHAPTER_1',
+    //       'Do': 'jump CHAPTER_1'
+    //     },
+    //     'CHAPTER_2': {
+    //       'Text': 'CHAPTER_2',
+    //       'Do': 'jump CHAPTER_2'
+    //     },
+    //     'CHAPTER_3': {
+    //       'Text': 'CHAPTER_3',
+    //       'Do': 'jump CHAPTER_3'
+    //     },
+    //     'CHAPTER_E': {
+    //       'Text': 'CHAPTER_E',
+    //       'Do': 'jump CHAPTER_E'
+    //     },
+    //     'END': {
+    //       'Text': 'END',
+    //       'Do': 'jump END'
+    //     }
+    //   }
+    // },
     'jump CHAPTER_0'
   ],
 
@@ -404,8 +408,13 @@ monogatari.script({
     'chap 0,',
     // start-of-CHAPTER-0-mark
     " <br><br>Prologue: Waxing Iridescen ",
-    "School hallway day <br><br>School hallway da ",
-    "Intro Music <br><br>Intro Musi ",
+
+    // "School hallway day <br><br>School hallway da ",
+    "show scene school_hallway_daylight",
+
+    // "Intro Music <br><br>Intro Musi ",
+
+
     "character_object_narration <br><br>It\'s lunch break at school, but the atmosphere is livelier than usual. Oh right, it\'s Club Day today. You still can\'t decide which club you want to join this year. ",
     "character_object_narration <br><br>You stop walking the hallways to look at the board of various club posters. Oh man, you\'ve never joined one before, so the amount of options overwhelm you a little. You think of just joining multiple clubs at a time, but you quickly brush off that thought. Clubs are supposed to be a good break from the humdrum of academics. Don\'t get ahead of yourself ",
     "character_object_narration <br><br>There\'s an art club poster. That\'s a hard no. The most artistic you\'ve ever been was when you colored the sky a solid green and the ground magenta. Only because the other crayons were missing. And that was in a coloring book. Not fun at all ",
@@ -416,11 +425,22 @@ monogatari.script({
     "character_object_narration <br><br>Ooh, a debate club poster. You don\'t consider yourself a chatterbox, but it\'s always fascinating to listen to other people\'s perspectives and find ways to decimate their arguments. It\'s basically a fun way to talk shit to others. Well, for the most part, save for some annoyingly serious people.. ",
     "character_object_narration <br><br>Damn, besides the debate club, it looks like you don\'t have many good options here ",
     "character_object_narration <br><br>Wait, what\'s with that poster over there? ",
-    "Radio club poster (wall poster). Allow toggle for zoom in to the RadioClub.png in Art folder. <br><br>Radio club poster (wall poster). Allow toggle for zoom in to the RadioClub.png in Art folder ",
+    
+    // "Radio club poster (wall poster). ",
+    'show image wallposter center with fadeIn',
+    // Allow toggle for zoom in to the RadioClub.png in Art folder. <br><br>Radio club poster (wall poster). Allow toggle for zoom in to the RadioClub.png in Art folder ",
+    
+    
     "character_object_narration <br><br>It advertises... a radio club? Interesting. Teachers love to praise your diction and confidence in your class presentations. This could be worth a try. ",
     "character_object_narration <br><br>You enter a nearby room with the radio club sign on its door. ",
-    "Music_Room_Basic <br><br>Music_Room_Basi ",
-    "character_object_kyo <br><br>Kyo_BP_Defaul ",
+
+
+    // "Music_Room_Basic <br><br>Music_Room_Basi ",
+    "show scene music_room_basic",
+    
+    // "character_object_kyo <br><br>Kyo_BP_Defaul ",
+    'show character character_object_kyo Kyo_BP_Default normal',
+
     "Centered <br><br>Centere ",
     "character_object_narration <br><br>There\'s only one student here. Yet, you can tell that you\'re in the right place. Her appearance is just as colorful as the poster, and the graffiti beside her only emphasizes that ",
     "character_object_narration <br><br>The microphone on the other side of the room looks too high for her to reach. That\'s a bit weird ",
@@ -428,14 +448,26 @@ monogatari.script({
     "character_object_player <br><br>Hi, I wanna join the radio club ",
     "character_object_narration <br><br>She puts her phone away and turns around ",
 
+
+    // 'show character character_object_kyo Kyo_FP_Shocked normal',
+    'show character character_object_kyo Kyo_FP_Shocked normal',
             "character_object_kyo <br><br>AAAAAAAAAAAAAAAAAAAAH!!  <br><br> ", // rule based 
    
             "show character character_object_kyo Kyo_FP_Shocked normal", // rule based 
  
                 "character_object_narration <br><br>The student\'s voice then drops a few octaves lower than you expect her to have ",
+    
+    
+    // 'show character character_object_kyo Kyo_FP_Bruh normal',
+    'show character character_object_kyo Kyo_FP_Bruh normal',
     "character_object_kyo <br><br>You fucking scared the shit out of me! My god. ",
+
+    
     "character_object_narration <br><br>Well, you now know two things. One, this girl is actually a guy. Two, this dude\'s language is as colorful as his hair. What a bombastic impression you\'ve left on each other. It only goes uphill from here ",
     "character_object_player <br><br>Sorry, I just walked in. Is this the wrong room? I think I\'m in the wrong ro&mdash ",
+
+    'show character character_object_kyo Kyo_FP_Default normal',
+
     "character_object_kyo <br><br>No no, wait. I made sure that the door had a sign on it. You\'re here for the radio club, right ",
     "character_object_player <br><br>Yeah, that\'s kinda what I said before you started freaking out. ",
     "character_object_kyo <br><br>Okay, okay, my bad. I literally didn\'t hear you walk in. Anyways, hi, I\'m Kyo Kaneko. What\'s your name ",
@@ -469,6 +501,11 @@ monogatari.script({
   ],
   'Chapter_0_Choice_1_Sure_you_can_have_a_bit': [
     "Choice 1 Sure, you can have a bit.  Chapter_0_0011400 Choice 1: Sure, you can have a bit. ",
+
+    
+    // 'show character character_object_kyo Kyo_FP_Bruh normal',
+    'show character character_object_kyo Kyo_FP_Bruh normal',
+
     "character_object_kyo Chapter_0_0011700 Actually, never mind. There are probably some RAAAAAAAANCID germs in there. I don't want any of that. I refuuuuuuuuuuse. ",
     "Narration  <br><br> The nerve of this guy...!  ",
     "Scene reconverges here  Chapter_0_0012200 Scene reconverges here ",
@@ -476,7 +513,10 @@ monogatari.script({
   ],
   'Chapter_0_Choice_2_No_this_is_mine_Get_your_own': [
     "Choice 2 No, this is mine. Get your own.  Chapter_0_0012500 Choice 2: No, this is mine. Get your own. ",
-    "character_object_kyo Chapter_0_0012700 Kyo_FP_Bruh ",
+
+    // "character_object_kyo Chapter_0_0012700 Kyo_FP_Bruh ",
+    'show character character_object_kyo Kyo_FP_Bruh normal',
+
     "Narration  <br><br> Kyo whines and pouts. 0 ",
     "Narration  <br><br> What is up with this guy? Yet, you find his pouting face a little cute. Like a puntable cat.   ",
     "Scene Reconverges Here  Chapter_0_0013300 Scene Reconverges Here ",
@@ -495,6 +535,10 @@ monogatari.script({
     // Chapter_0_0013000 removed - covered in choice block above 
     // Chapter_0_0013200 removed - covered in choice block above 
     // Chapter_0_0013300 removed - covered in choice block above 
+
+
+
+    'show character character_object_kyo Kyo_FP_Default normal',
     "character_object_kyo <br><br>Anyways, since this club requires confident people, I\'d say you\'re a pretty good candidate so far. Most people don\'t have the balls to approach me ",
     "character_object_narration <br><br>You look up and down at Kyo incredulously ",
     "character_object_player <br><br>Really? You\'re not even tall enough to reach the mic over there ",
@@ -519,7 +563,11 @@ monogatari.script({
 
   'Chapter_0_Choice_2_Yes': [
     "Choice 1 Yes.  Chapter_0_0015500 Choice 1: Yes. ",
-    "character_object_kyo Chapter_0_0015700 Kyo_FP_Cheerful ",
+    
+    // "character_object_kyo Chapter_0_0015700 Kyo_FP_Cheerful ",
+    'show character character_object_kyo Kyo_FP_Cheerful normal',
+
+
     "Narration  <br><br> Kyo claps his hands. <br><br>  ",
     "character_object_kyo Chapter_0_0016300 Yeah, let's go! Finally, there's a second member. ",
     "Player  <br><br> Wait, nobody else is in this club yet?   ",
@@ -539,19 +587,38 @@ monogatari.script({
   ],
   'Chapter_0_Choice_2_No': [
     "Choice 2 No.  Chapter_0_0020100 Choice 2: No. ",
+
+    'show character character_object_kyo Kyo_FP_Sad normal',
     "character_object_kyo Chapter_0_0020400 Oh, why not?  ",
+
+
+
     "Player  <br><br> Oh, don't take this the wrong way. It's just that I wanna consider my other options.   ",
     "character_object_kyo Chapter_0_0021000 Like what? ",
     "Player  <br><br> Hm? Isn't it normal to do that?   ",
     "character_object_kyo Chapter_0_0021600 What other clubs were you thinking of joining? Because this club can do better than all of them. ",
     "Narration  <br><br> Where did this sudden pushiness and big ego come from?   ",
     "Player  <br><br> I was thinking of joining the debate club'  ",
+
+
+    'show character character_object_kyo Kyo_FP_Bruh normal',
     "character_object_kyo Chapter_0_0022500 Bro, I am right here. I am THE debate master. I'm not all business all the time you know. I can do other things outside of club meetings.  ",
+   
+   
+   
+   
     "Player  <br><br> Okay, but I have to ask. You're cool and all, but are you the only club member?   ",
     "character_object_kyo Chapter_0_0023100 Yes, but I'm gonna get more soon' ",
     "Player  <br><br> Look, I'll go check out other clubs later this week. If I don't like any of them, I'll come back here. Okay?  ",
     "Narration  <br><br> The lunch bell rings.  ",
+
+
+    'show character character_object_kyo Kyo_SP_Sad normal',
     "Player  <br><br> Don't wanna be late to class. See ya.  ",
+
+
+
+
     "character_object_kyo Chapter_0_0024200 Kyo_BP_Sad ",
     "Narration  <br><br> You feel a little guilty seeing his dejected expression as you leave the classroom. But it's too late now; you stick to your decision and go to your next class.  ",
     "Empty black screen.  Chapter_0_0024700 Empty black screen. ",
